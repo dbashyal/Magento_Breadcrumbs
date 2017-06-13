e15cb6e2e12a3510a5e649cff45045d2bb469794
dbashyal <dbashyal@gmail.com>
2017-06-13 10:04:37 +1000

    Updated breadcrumb to get deepest category

--- app/code/community/Technooze/Breadcrumbs/Model/Observer.php
+++ app/code/community/Technooze/Breadcrumbs/Model/Observer.php
@@ -8,18 +8,61 @@
 */
 class Technooze_Breadcrumbs_Model_Observer
 {
-	/**
-	* Adds category to breadcrumb for product views 
-	* originating from outside a category, for instance
-	* homepage or navigation links.
-	*
-	* @return Technooze_Breadcrumbs_Model_Observer
-	*/
-	public function addCategoryBreadcrumb(Varien_Event_Observer $observer)
+
+    const LAYERED_FILTER_CATEGORY_PARAM = 'cat';
+
+    /**
+     * Show proper category breadcrumb for layered navigation
+     * When filtering by sub-category
+     * @param Varien_Event_Observer $observer
+     * @return $this
+     */
+    public function catalogControllerCategoryInitAfter(Varien_Event_Observer $observer)
+    {
+        /* @var $category Mage_Catalog_Model_Category */
+        $category = $observer->getCategory();
+        $cat = Mage::app()->getRequest()->getParam(self::LAYERED_FILTER_CATEGORY_PARAM, 0);
+        if(!$cat){
+            return $this;
+        }
+        $cat = (int)$cat;
+
+        if($cat != $category->getId()){
+            $cat = Mage::getSingleton('catalog/category')->setStoreId($category->getStoreId())->load($cat);
+
+            if(!$cat->getId()){
+                return $this;
+            }
+
+            Mage::unregister('current_category');
+            Mage::register('current_category', $cat);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds category to breadcrumb for product views
+     * originating from outside a category, for instance
+     * homepage or navigation links.
+     * @param Varien_Event_Observer $observer
+     * @return $this
+     */
+    public function addCategoryBreadcrumb(Varien_Event_Observer $observer)
 	{
-		if (!Mage::registry('current_category')) {
-            /* @var $product Mage_Catalog_Model_Product */
-			$product = $observer->getProduct();
+        /* @var $product Mage_Catalog_Model_Product */
+        $product = $observer->getProduct();
+        $isSchoolWear = $product->getIsSchoolWear(); // Modify this as your need.
+        $category = Mage::registry('current_category');
+
+		if (!$category || $isSchoolWear) {
+		    if($category && $category->getId()){
+                $pathIds = $category->getPathIds();
+
+                if(count($pathIds) >= 4){ // for me minimum depth required is 4 for a school category page.
+                    return $this;
+                }
+            }
 			$categoryIds = $product->getCategoryIds();
 
 			if(count($categoryIds)) {
@@ -28,13 +71,15 @@ class Technooze_Breadcrumbs_Model_Observer
 				$collection->addAttributeToSelect('name');
 				$collection->addAttributeToFilter('is_active', array('eq'=>'1'));
 				$collection->addAttributeToFilter('entity_id', array('in' => $categoryIds));
-				$collection->addAttributeToSort('entity_id', 'desc');
+				$collection->addAttributeToSort('level', 'desc');
 				$collection->setPageSize(1);
 
                 /* @var $category Mage_Catalog_Model_Category */
                 $category = $collection->getFirstItem();
-				if ($category->getId()) {
+
+                if ($category->getId()) {
 					$product->setCategory($category);
+					Mage::unregister('current_category'); // unregister existing category
 					Mage::register('current_category', $category);
 				}
 			}
